a3ea72ec435c84af7de7bc9f99c3375e618c9455
undreew <jamesdejesus74@gmail.com>
2025-08-07 16:17:49 +0800

    add a simple UserDTO for a return structure

--- src/main/java/com/james/store/controllers/UserController.java
+++ src/main/java/com/james/store/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.james.store.controllers;
 
+import com.james.store.dtos.UserDto;
 import com.james.store.entities.User;
 import com.james.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -18,18 +19,19 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository.findAll().stream().map(u -> new UserDto(u.getId(), u.getName(), u.getEmail())).toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         var user =  userRepository.findById(id).orElse(null);
         
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
 
-        return ResponseEntity.ok(user);
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
--- src/main/java/com/james/store/repositories/UserRepository.java
+++ src/main/java/com/james/store/repositories/UserRepository.java
@@ -2,9 +2,10 @@ package com.james.store.repositories;
 
 import com.james.store.entities.User;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
