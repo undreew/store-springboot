f9d3e3dc2bc8abd161d9341ded169fe0f0917983
undreew <jamesdejesus74@gmail.com>
2025-08-07 15:13:12 +0800

    add Home and User controller

---
+++ src/main/java/com/james/store/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.james.store.controllers;
+
+import com.james.store.entities.User;
+import com.james.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        var user =  userRepository.findById(id).orElse(null);
+        
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
+    }
+}
