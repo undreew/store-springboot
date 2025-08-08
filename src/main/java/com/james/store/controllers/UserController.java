8f315269729747b4b1f0b623c9175c4c57482829
undreew <jamesdejesus74@gmail.com>
2025-08-08 13:48:19 +0800

    accepting request parameters

--- src/main/java/com/james/store/controllers/UserController.java
+++ src/main/java/com/james/store/controllers/UserController.java
@@ -4,11 +4,11 @@ import com.james.store.dtos.UserDto;
 import com.james.store.mappers.UserMapper;
 import com.james.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -19,8 +19,9 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll().stream().map(userMapper::toDto).toList();
+    public Iterable<UserDto> getAllUsers(@RequestParam(required = false, defaultValue = "", name = "sort") String sortBy) {
+        if (!Set.of("name", "email").contains(sortBy)) sortBy = "name";
+        return userRepository.findAll(Sort.by(sortBy)).stream().map(userMapper::toDto).toList();
     }
 
     @GetMapping("/{id}")
