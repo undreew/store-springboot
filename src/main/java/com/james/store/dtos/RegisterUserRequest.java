3249117948be45372e118ae90c67f6cb2064b782
james <jamesdejesus74@gmail.com>
2025-08-10 12:35:42 +0800

    add custom validations by creating custom annotations and constraints

--- src/main/java/com/james/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/james/store/dtos/RegisterUserRequest.java
@@ -1,5 +1,7 @@
 package com.james.store.dtos;
 
+import com.james.store.validation.Lowercase;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -13,6 +15,7 @@ public class RegisterUserRequest {
 
   @NotBlank(message = "Email is required")
   @Email(message = "Email must be valid")
+  @Lowercase(message = "Email must be in lowercase")
   private String email;
 
   @NotBlank(message = "Password is required")
