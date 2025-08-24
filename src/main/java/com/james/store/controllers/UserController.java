0f1a8a093546ca30e5df4129bd5b8ce8ba35a178
james <jamesdejesus74@gmail.com>
2025-08-24 17:47:18 +0800

    add hashing using bcrypt

--- src/main/java/com/james/store/controllers/UserController.java
+++ src/main/java/com/james/store/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +35,7 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/users")
 public class UserController {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
@@ -66,6 +68,7 @@ public class UserController {
         }
 
         var user = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
 
         var userDto = userMapper.toDto(user);
